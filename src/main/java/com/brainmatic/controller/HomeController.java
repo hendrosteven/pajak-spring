8565643f250bbcd7c0566ed867aabbcc58fbfaab
Hendro Steven <hendro.steven@gmail.com>
2018-05-22 16:13:52 +0800

    update layout

--- src/main/java/com/brainmatic/controller/HomeController.java
+++ src/main/java/com/brainmatic/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.brainmatic.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +21,7 @@ public class HomeController {
 	@RequestMapping(method=RequestMethod.GET)
 	public String index(Model model) {
 		Iterable<Movie> movies = movieService.findAll();
-		model.addAttribute("listMovie", movies);
+		model.addAttribute("listMovie", movies); 
 		return "index";
 	}
 }
