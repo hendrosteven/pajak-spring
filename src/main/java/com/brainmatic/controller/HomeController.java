dbe44661073db7cf5ba2539b38414a5ab24cc4ab
Hendro Steven <hendro.steven@gmail.com>
2018-05-23 09:27:46 +0800

    add genre to the home page

--- src/main/java/com/brainmatic/controller/HomeController.java
+++ src/main/java/com/brainmatic/controller/HomeController.java
@@ -1,14 +1,12 @@
 package com.brainmatic.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.brainmatic.model.Movie;
+import com.brainmatic.service.GenreService;
 import com.brainmatic.service.MovieService;
 
 @Controller
@@ -17,11 +15,13 @@ public class HomeController {
 	
 	@Autowired
 	private MovieService movieService;
+	@Autowired
+	private GenreService genreService;
 
 	@RequestMapping(method=RequestMethod.GET)
 	public String index(Model model) {
-		Iterable<Movie> movies = movieService.findAll();
-		model.addAttribute("listMovie", movies); 
+		model.addAttribute("listMovie", movieService.findAll()); 
+		model.addAttribute("listGenre", genreService.findAll());
 		return "index";
 	}
 }
